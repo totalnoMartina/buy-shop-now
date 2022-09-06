516a670edf98fd40ef89a95550e6832b72e1ad72
totalnoMartina <totalnobzvz@yahoo.com>
2022-09-06 23:12:38 +0000

    Error on allowed hosts so tried changing it according to the error suggestion

--- macrameproject/settings.py
+++ macrameproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['macrameapp2022.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['macrameapp2022.herokuapp.com', 'localhost']
 
 
 # Application definition
